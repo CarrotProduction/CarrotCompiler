ea5e6edba3543e24cdb2823c6ec22dc8d8b713de
walkalone20 <73780235+walkalone20@users.noreply.github.com>
2023-06-08 15:34:37 +0800

    Update register.cpp

--- src/riscv/register.cpp
+++ src/riscv/register.cpp
@@ -1,5 +1,34 @@
 #include "register.h"
+#include "ir.h"
 
-void addrMapping() {
+// 初始化寄存器堆
+void InitRegFile() {
+  // 整型
+  for (int i = 0; i < REG_NUMBER; i++) {
+    std::string cur = "r" + std::to_string(i);
+    Int.push_back(Register(&Type(Type::IntegerTyID), Register::RegType::UND, cur));
+  }
+  // 浮点
+  for (int i = 0; i < REG_NUMBER; i++) {
+    std::string cur = "f" + std::to_string(i);
+    Float.push_back(Register(&Type(Type::FloatTyID), Register::RegType::UND, cur));
+  }
+}
 
+// 优化后的映射
+void OptimizedAddrMapping() {
+
+}
+
+// 无优化映射
+void NormalAddrMapping() {
+
+}
+
+void addrMapping(int op) {
+  InitRegFile();
+  if (op)
+    OptimizedAddrMapping();
+  else
+    NormalAddrMapping();
 }

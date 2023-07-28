b3c3cad85b06e7a9035a6775dc716da9120c5d14
Seas0 <season1277@outlook.com>
2023-07-28 16:05:34 +0800

    [Backend][WIP]: Add .global specifier to function

--- src/riscv/riscv.cpp
+++ src/riscv/riscv.cpp
@@ -13,7 +13,9 @@ RiscvFunction::RiscvFunction(std::string name, int num_args,
 // 由于一个函数可能有若干个出口，因而恢复现场的语句根据basic block
 // 语句中的ret语句前面附带出现，因而不在此出现
 std::string RiscvFunction::print() {
-  std::string riscvInstr = this->name_ + ":\n"; // 函数标号打印
+  // TODO: temporaily add '.global' to declare function
+  // Don't know if '.type' is needed
+  std::string riscvInstr = ".global " + this->name_ + "\n" + this->name_ + ":\n"; // 函数标号打印
   // 对各个basic block进行拼接
   for (auto x : this->blk)
     riscvInstr += x->print();
